9ca50faa38c5d8a526963f082fedefd33c2c6fe3
Patricia Rocha <patirocha.dev@gmail.com>
2023-11-28 20:00:28 -0300

    created project

---
+++ src/main/java/com/patriciarocha/pharmapdvapi/PharmapdvapiApplication.java
@@ -0,0 +1,13 @@
+package com.patriciarocha.pharmapdvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmapdvapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmapdvapiApplication.class, args);
+	}
+
+}
